d75ffd8dda18ff5e807ca7c528ea72daa0eca876
Peter Lesslie <pclesslie@gmail.com>
2016-09-05 16:17:13 -0500

    Add more test positions

--- main.c
+++ main.c
@@ -1228,19 +1228,25 @@ int main(int argc, char **argv) {
     printf("Built in `debug' mode\n");
 #endif
 
-    //#define DEPTH 6
-    // verified up to depth 7 (9/4/16)
+    #define DEPTH 3
+    
     // starting position:
     //const char *fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -";
 
-    // verified up to depth 3 (9/4/16)
     // kiwi pete position:
-    const char *fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -";
-    #define DEPTH 4
+    /* const char *fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -"; */
 
     // position #3
     //const char *fen = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -";
-    //const char *fen = "8/2p5/3p4/KP5r/5p1k/8/4P1P1/1R6 b - -";
+
+    // position #4
+    //const char *fen = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq -";
+
+    // position #5
+    //const char* fen = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ -";
+
+    // position #6
+    const char* fen = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - -";
     
     printf("Perft:\n");
     if (read_fen(&pos, fen) != 0) {
