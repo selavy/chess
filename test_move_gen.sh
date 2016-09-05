7558867d1c196aac00585664832d76e236c08c9c
Peter Lesslie <pclesslie@gmail.com>
2016-09-05 13:46:07 -0500

    Remove unused scripts

--- test_move_gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-make && \
-    ./chess > ccout && \
-    sort ccout > ccout.sorted && \
-    ./perft.py > pyout && \
-    sort pyout > pyout.sorted && \
-    diff ccout.sorted pyout.sorted > differences && \
-    ./print_fake_fen_position.py differences
