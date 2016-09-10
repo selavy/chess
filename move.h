c8a793d23ff1196b4ada229576848bb110cba3e6
Peter Lesslie <pclesslie@gmail.com>
2016-09-10 14:00:37 -0500

    Begin defining `smallmove' form

--- move.h
+++ move.h
@@ -38,4 +38,38 @@ extern void mprnt(move m);
 
 extern int is_castle(move m);
 
+#define SM_TRUE       1
+#define SM_FALSE      0
+#define SM_PRM_NONE   (SM_FALSE)
+#define SM_PRM_KNIGHT 0
+#define SM_PRM_BISHOP 1
+#define SM_PRM_ROOK   2
+#define SM_PRM_QUEEN  3
+typedef uint16_t smallmove_t;
+#ifdef NDEBUG
+#define SMALLMOVE(to, from, prm, ep, csl)       \
+    (((to)   & 0x3f) <<  0) |                   \
+    (((from) & 0x3f) <<  6) |                   \
+    (((prm)  & 0x02) << 12) |                   \
+    ((!!(ep)*1 + !!(prm)*2 + !!(csl)*3) << 14)
+#else
+uint16_t SMALLMOVE(int to, int from, int prm, int ep, int csl) {
+    assert(ep  == SM_TRUE || ep  == SM_FALSE);
+    assert(csl == SM_TRUE || csl == SM_FALSE);
+    assert(prm == SM_PRM_NONE ||
+           prm == SM_PRM_KNIGHT ||
+           prm == SM_PRM_BISHOP ||
+           prm == SM_PRM_ROOK ||
+           prm == SM_PRM_QUEEN);
+    assert(ep == 0 || (prm == 0 && csl == 0));
+    assert(prm == 0 || (ep == 0 && csl == 0));
+    assert(csl == 0 || (ep == 0 && prm == 0));
+
+    return ((((to)   & 0x3f) <<  0) |                  
+            (((from) & 0x3f) <<  6) |                   
+            (((prm)  & 0x02) << 12) |                   
+            ((!!(ep)*1 + !!(prm)*2 + !!(csl)*3) << 14));
+}
+#endif
+
 #endif // MOVE__H_
