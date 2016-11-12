dc1cbc33fcb9308696b81b27acc37e82eed2f450
Peter Lesslie <pclesslie@gmail.com>
2016-11-12 14:44:55 -0600

    Switch fen iteration method

--- version2/main.c
+++ version2/main.c
@@ -7,31 +7,25 @@
 int main(int argc, char **argv) {
     int ret;
     struct position pos;
-    /* const char *starting_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"; */
-    /* ret = position_from_fen(&pos, starting_position); */
-    /* if (ret != 0) { */
-    /* 	fprintf(stderr, "Unable to read fen for position: %d!\n", ret); */
-    /* 	exit(EXIT_FAILURE); */
-    /* } */
-    /* position_print(stdout, &pos); */
-
     const char *fen[] = {
 	"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
 	"rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",
 	"rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2",
-	"rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
+	"rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2",
+	0
     };
-    const size_t nfen = sizeof(fen) / sizeof(*fen);
+    const char **cur = &fen[0];
 
-    for (int i = 0; i < nfen; ++i) {
-	ret = position_from_fen(&pos, fen[i]);
+    while (*cur) {
+	ret = position_from_fen(&pos, *cur);
 	if (ret != 0) {
 	    fprintf(stderr, "Unable to read fen for position! Error(%d), FEN = %s\n",
-		    ret, fen[i]);
+		    ret, *cur);
 	    exit(EXIT_FAILURE);
 	}
 	position_print(stdout, &pos);
 	printf("\n");
+	++cur;
     }
     
     return EXIT_SUCCESS;
