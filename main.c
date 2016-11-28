4fe95253a49c620d2a70b56797be96ea80183d43
Peter Lesslie <pclesslie@gmail.com>
2016-11-27 18:53:33 -0600

    Test perfts for new legal move gen code

--- main.c
+++ main.c
@@ -194,7 +194,7 @@ int main(int argc, char **argv) {
     }
     #endif
 
-    #if 1
+    #if 0
     // verify perft values on some known positions
     printf("checking perft values...\n");
     if (check_perft() != 0) {
@@ -204,9 +204,9 @@ int main(int argc, char **argv) {
     }
     #endif
 
-    #if 0
+    #if 1
     // time starting position perft to given depth
-    time_test(8);
+    time_test(7);
     #endif
 
     #if 0
